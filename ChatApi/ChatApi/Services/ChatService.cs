fed0c323d26f88f2a3304a10f4ebadd41d0db4ab
Doug Baek <baekdoug@gmail.com>
2021-08-16 12:51:38 -0500

    - added implementation

--- ChatApi/ChatApi/Services/ChatService.cs
+++ ChatApi/ChatApi/Services/ChatService.cs
@@ -1,7 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChatApi.Model;
+using MySqlConnector;
+
 namespace ChatApi.Services
 {
-    public class ChatService
+    public class ChatService : IChatService
     {
-        
+        private AppDb _db;
+
+        public ChatService(AppDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<ChatMessage> RetrieveMessageById(int id)
+        {
+            return new ChatMessage
+            {
+                Id = id
+            };
+        }
+
+        public async Task<IEnumerable<ChatMessage>> RetrieveAllMessagesByUser(string username)
+        {
+            var finalResult = new List<ChatMessage>();
+            await using var query = _db.Connection.CreateCommand();
+            if (query.Connection != null) await query.Connection.OpenAsync();
+            query.CommandText = @$"SELECT * FROM `logs` WHERE username = '{username}'";
+            var result = await query.ExecuteReaderAsync();
+            while(result.Read())
+            {
+                finalResult.Add(new ChatMessage
+                {
+                    Id = result.GetInt32(0),
+                    Username = result.GetString(1),
+                    Text = result.GetString(2),
+                    Timeout = result.GetString(3)
+                });
+            }
+
+            return finalResult;
+        }
+
+        public async Task<string> CreateNewMessage(ChatMessage chatMessage)
+        {
+            await using var query = _db.Connection.CreateCommand();
+            if (query.Connection != null) await query.Connection.OpenAsync();
+            query.CommandText = @$"INSERT INTO `logs` (username, text, timeout) values ('{chatMessage.Username}', '{chatMessage.Text}', '{chatMessage.Timeout}')";
+            await query.ExecuteNonQueryAsync();
+            query.Connection?.Close();
+            return "Ok";
+
+        }
     }
 }
---
+++ ChatApi/ChatApi/Services/IChatService.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChatApi.Model;
+
+namespace ChatApi.Services
+{
+    public interface IChatService
+    {
+        public Task<ChatMessage> RetrieveMessageById(int id);
+        public Task<IEnumerable<ChatMessage>> RetrieveAllMessagesByUser(string username);
+        public Task<string> CreateNewMessage(ChatMessage chatMessage);
+    }
+}
